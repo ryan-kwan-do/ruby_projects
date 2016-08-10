e00005ac82b4267159f4edc9fc8d4fb164eaa53a
Ryan <hello@ryanarmstrong.me>
2016-08-10 11:22:32 +0200

    completed iteration 4

--- files_and_serialization/event_manager/lib/event_manager.rb
+++ files_and_serialization/event_manager/lib/event_manager.rb
@@ -1,34 +1,44 @@
 require "csv"
 require "sunlight/congress"
+require "erb"
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
-
-puts "Event manager initialized!"
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
 end
 
 def legislators_by_zipcode(zipcode)
-  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-
-  legislator_names = []
-  legislator_names = legislators.collect do |legislator|
-    "#{legislator.first_name} #{legislator.last_name}"
-  end
-
-  legislator_names.join(", ")
-  
+  Sunlight::Congress::Legislator.by_zipcode(zipcode)
 end
 
-contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
-contents.each do |row|
-  name = row[:first_name]
+def save_thank_you_letters(id,form_letter)
+  Dir.mkdir("output") unless Dir.exists?("output")
 
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename,'w') do |file|
+    file.puts form_letter
+  end
+end
+
+
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
+
+#begin control flow
+puts "Event manager initialized!"
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
 
-  puts "#{name} #{zipcode} #{legislators_by_zipcode(zipcode)}"
+  legislators = legislators_by_zipcode(zipcode)
 
+  form_letter = erb_template.result(binding)
 
+  #save_thank_you_letters(id, form_letter)
 end
 
