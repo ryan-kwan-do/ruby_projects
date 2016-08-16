bb772ef44a9bebd49d4cd9c89249e453d61bf538
Ryan <hello@ryanarmstrong.me>
2016-08-16 11:25:05 +0200

    initial hangman commit

---
+++ files_and_serialization/hangman/lib/hangman/game.rb
@@ -0,0 +1,12 @@
+module Hangman
+  class Game
+    def initialize(output) #word = Word.new)
+      @output = output
+      @word
+    end
+
+    def start_new_game
+      
+    end
+  end
+end
---
+++ files_and_serialization/hangman/spec/hangman/game_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+module Hangman
+  describe Game do
+  	let(:display_output) { double('display_output').as_null_object }
+  	let(:game) { Game.new(display_output)}
+
+  	describe '#start_new_game' do
+  	  it 'displays a welcome message' do
+  	  	game.start_new_game
+  	  	expect(display_output).to receive(:puts).with('Welcome to Hangman!')
+  	  end
+  	  it 'displays a blank board'
+  	  it 'displays the remaining number of incorrect guesses'
+  	end
+  	describe '#player_guess' do
+  	  context 'one letter correct'
+  	  context 'more than one letter correct'
+  	  context 'no letters correct'
+  	end
+  	describe '#player_lose'
+  	describe '#player_win'
+
+  end
+end
